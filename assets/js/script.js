eef78833b38a62ce16b20b05e3a39e9a02fd8052
daisybutler <daisybutler16@gmail.com>
2021-09-18 10:18:57 +0000

    Added toggle functionality to info circle hover.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
+
     // Change the nav dropdown icon to a close icon when open
     $('#nav-collapse-btn').click(function(){
         $(this).toggleClass('navbar-toggler');
         $(this).toggleClass('btn-close');
         $('#toggler-icon').toggleClass('navbar-toggler-icon');
     });
+
     // Hover behaviour of section 2 info circles
     $('.circle').mouseenter(function(){
         $(this).css("background-color", "rgba(2, 103, 107,.5)");
+        $(this).children('.circle h3').addClass("d-none");
+        $(this).children('.circle-hover-text').toggleClass("d-none");
     });
     $('.circle').mouseleave(function(){
         $(this).css("background-color", "#02676B");
+        $(this).children('.circle h3').removeClass("d-none");
+        $(this).children('.circle-hover-text').toggleClass("d-none");
     });
 });
